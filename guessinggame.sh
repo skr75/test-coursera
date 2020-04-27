4f1197a8c2c8e88097e307d89891050b3af0ba11
skr75 <64161263+skr75@users.noreply.github.com>
2020-04-27 11:23:51 +0200

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #this is my first ever try to code, hope it works as I could only test online
 #be gentle :)
-function test_coursera {  #this declares the Function
+function test_coursera {                                      #this declares the Function
 	
 	check=0	
 	x=$(ls -al | grep -c)
-	while [[ $check -eq 0 ]] #this starts the loop
+	while [[ $check -eq 0 ]]                               #this starts the loop
 	do
 	   echo "Guess the number of files in directory:"
-	   read -r guess
-	   if ! [[ $guess =~ ^[0-9]+$ ]] #this starts if statement
+	   read -r guess                                       #this is reading the input from user
+	   if ! [[ $guess =~ ^[0-9]+$ ]]                       #this starts if statement
 	   then 
 	      echo "Numer not valid, please retry"
 	   elif [[ $guess ==  $x ]]
